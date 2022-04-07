0215cfc77f604b31e631a2f374b5d2b9bcf01c17
Jake Bladt <jake@jakebladt.com>
2022-04-07 18:02:26 -0400

    Initial commit

---
+++ efmigrate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace efmigrate
+{
+    class Program
+    {
+
+        private static AmazonDynamoDBClient _client;
+        private static DynamoDBContext _context;
+
+        static void Main(string[] args)
+        {
+            _client = new AmazonDynamoDBClient();
+            _context = new DynamoDBContext(_client);
+            var search = LoadSubjectsAsync().Result;
+            var allSubjects = search.GetRemainingAsync().Result;
+
+            allSubjects.ForEach(s => Console.WriteLine(s.FilePrefix));
+        }
+
+        static async Task<AsyncSearch<Subject>> LoadSubjectsAsync()
+        {
+            var c = new ScanCondition("FilePrefix", ScanOperator.IsNotNull);
+            var conditions = new ScanCondition[] { c };
+            return await Task.FromResult(_context.ScanAsync<Subject>(conditions));
+        }
+    }
+}
---
+++ efmigrate/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Amazon.DynamoDBv2.DataModel;
+
+namespace efmigrate
+{
+    [DynamoDBTable("GallerySubjects")]
+    public class Subject
+    {
+        [DynamoDBHashKey]
+        public string FilePrefix { get; set; }
+        [DynamoDBProperty]
+        public string DisplayName { get; set; }
+        [DynamoDBProperty]
+        public int ImageCount { get; set; }
+    }
+}
